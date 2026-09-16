4ec5b65e98b4d06074d8c2520b5dfed30d4af72e
Gabriel Nalesso Leal <00115366@unisoacad.uniso.br>
2026-09-15 21:48:01 -0300

    final aula 15/9

--- paginas/boasvindas.php
+++ paginas/boasvindas.php
@@ -1,3 +1,13 @@
+<?php
+
+    session_start();
+    if (isset($_SESSION["usuario"])){
+        header("location: index.php");
+        exit();
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -9,19 +19,25 @@
 <body>
     <div class="container">
     <h1>OLá TerRaQuiO</h1>
-    <h2>SeJA Bem VinDo</h2>
+    <p><h2>SeJA Bem VinDo a EstE PLAnETA!</h2>
     <img src="../img/OVINI.jpg" alt="Minha Imagem">
     <br><br>
     <?php
-        echo "PROJETO INTEGRADOR";
-        echo"<br><br>";
-        echo"Olá mundo!!! Eu venho em Paz.";
-        echo"<br><br>";
-        echo"Thomas -----> ";
-        $Thomas = "Batata";
-        echo $Thomas;
-    ?>
+        echo htmlspecialchars($_SESSION["usuario"], ENT_QUOTES, 'UTF-8');  
+    ?></p>
     <p>EsTe é O meU PrImEiRo pRoJETo</p>
+    <br><br>
+
+    <o class="botao-sair" href="logout.php">Sair</o>
+
+    <section class ="thingspeak">
+
+        <h2>Dados do thingspeak</h2>
+        <a href="https://thingspeak/channels/seu_canal_id" target="_blank" rel="depois">
+            <img src="https://thingspeak.com/channels/seu_canal_id/badges/l.svg" alt="badge do thingspeak">
+        </a>
+    </section>
+
     </div>
 </body>
 </html>
